d0b4a14df36fb3cd55e31c4a97cdfdd089ebd4fa
Erick Rivera <yomellamoerick.771@gmail.com>
2023-07-06 07:44:08 -0400

    the users update pt1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
     def after_sign_in_path_path_for(resource)
         publications_path
+    end
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :age, :phone])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :age, :phone])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,18 @@ Rails.application.routes.draw do
     resources :comments, only: [:create] #:destroy
   end
 
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions', #ESTA VEZ TAMBIÉN ME SALTE LA COMA EN SERIO SOY LA SAL DE LA TIERRA
+    registrations: 'users/registrations'
+  }
 
   resources :users do
     resources :comments
     resources :publications
   end
+  
   get 'homes/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
   root 'publications#index'
   get '/publications', to: 'publications#index', as: 'user_root'
 end
